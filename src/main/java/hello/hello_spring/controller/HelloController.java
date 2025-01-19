0460e55e805227cfc4c31cdf79ddaf4da7b8f4c3
juyounglee08 <coderjuyoung82@gmail.com>
2025-01-20 00:51:37 +0900

    2nd 커밋. vmc(view-model-controller) 방식 구현 및 주석 수정.

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -2,14 +2,21 @@ package hello.hello_spring.controller;
 
 import org.springframework.ui.Model; //여기서 model은 view와 Controller 간의 데이터 전달을 돕는 객체 역할을 수행합니다.
 import org.springframework.stereotype.Controller; //Controller는 화면(view)에서 요청한 주소를 받아들여 어디로 갈 지 분석하고 맞는 길(business logic)로 연결시켜주는 다리 역할을 수행합니다.
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping; //GetMapping은 요청주소와 실제주소를 Mapping 하도록(해당 값이 다른 값을 가르키도록)하는 어노테이션입니다.
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hello") //GetMapping은 요청주소와 실제주소를 Mapping 하도록(해당 값이 다른 값을 가르키도록)하는 어노테이션입니다.
+    @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!"); //addAttribute("String name","Object value")을 통해 value 객체가 name으로 추가되고, view에서 name으로 지정된 value를 사용하게 됩니다.
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { //RequestParam은 파라미터 이름으로 바운딩()되는 방법으로, 요청 파라미터를 매우 편리하게 사용할 수 있습니다.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
